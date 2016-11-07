ecb046e96e832841a6dc5403a14b13ee035bb35a
andrewalc <andrewalc98@gmail.com>
2016-11-07 03:33:34 -0500

    Added locals
    Cleaned up code using locals

--- wormgame.rkt
+++ wormgame.rkt
@@ -29,11 +29,11 @@
 ;; are numbers representing the x-coord and y-ccord respectively
 ;; dir is a Direction representing the worms movement direction
 
-;; A Directions is one of:
-; - "up"
-; - "down"
-; - "left"
-; - "right"
+;; Initial Worm:
+(define init-worm (make-worm 1 (make-posn (/ HEIGHT 2) (/ WIDTH 2)) "right"))
+
+;; A Direction is a member of:
+(define directions '("up" "down" "left" "right"))
 
 ;-------------------------------------------------------------------------------
 ; FUNCTIONS:
@@ -49,36 +49,39 @@
 ;; move: Worm -> Worm
 ;; move: Changes the worm's position everytick, depending on Worm's direction
 (define (move w)
-  (local ((define x-pos (posn-x (worm-pos w)))
-          (define y-pos (posn-y (worm-pos w))))
-    (cond [(string=? (worm-dir w) "right")
-           (make-worm 1 (make-posn (+ x-pos w-unit) y-pos) (worm-dir w))]
-          [(string=? (worm-dir w) "left")
-           (make-worm 1 (make-posn (- x-pos w-unit) y-pos) (worm-dir w))]
-          [(string=? (worm-dir w) "up")
-           (make-worm 1 (make-posn x-pos (- y-pos w-unit)) (worm-dir w))]
-          [(string=? (worm-dir w) "down")
-           (make-worm 1 (make-posn x-pos (+ y-pos w-unit)) (worm-dir w))])))
+  (local ((define w-size         (worm-size w))
+          (define x-pos          (posn-x (worm-pos w)))
+          (define y-pos          (posn-y (worm-pos w)))
+          (define w-dir          (worm-dir w))
+          (define (dir? str dir) (string=? str dir))
+          (define (up? str)      (dir? str "up"))
+          (define (down? str)    (dir? str "down"))
+          (define (left? str)    (dir? str "left"))
+          (define (right? str)   (dir? str "right")))
+    (cond [(up? w-dir)
+           (make-worm w-size (make-posn x-pos (- y-pos w-unit)) w-dir)]
+          [(down? w-dir)
+           (make-worm w-size (make-posn x-pos (+ y-pos w-unit)) w-dir)]
+          [(left? w-dir)
+           (make-worm w-size (make-posn (- x-pos w-unit) y-pos) w-dir)]
+          [(right? w-dir)
+           (make-worm w-size (make-posn (+ x-pos w-unit) y-pos) w-dir)]
+          [else w])))
 
 ;; keypress: Worm KeyEvent -> Worm
 ;; keypress: Changes given Worm's direction to the respective arrow key pressed
 (define (keypress w key)
   (local ((define x-pos (posn-x (worm-pos w)))
-          (define y-pos (posn-y (worm-pos w))))
-    (make-worm (worm-size w) (make-posn x-pos y-pos) key)))
+          (define y-pos (posn-y (worm-pos w)))  )
+    (cond [(member? key directions)
+           (make-worm (worm-size w) (make-posn x-pos y-pos) key)]
+          [else w])))
 
 ;; worm-game: X -> Worm
 ;; worm-game: Worm: a game based on the game Snake
 (define (worm-game x)
-  (big-bang (make-worm 1 (make-posn (/ HEIGHT 2) (/ WIDTH 2)) "right")
+  (big-bang init-worm
             [to-draw render]
             [on-key keypress]
             [on-tick move .5]))
 ;-------------------------------------------------------------------------------
-
-
-
-
-
-
-
